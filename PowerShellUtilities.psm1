180ea47e475a0567963d6225b207820efdcad5ae
Dan Simpson <daniel_simpson1988@hotmail.com>
2014-06-20 09:04:11 +1000

    Updated generic Utilities

--- PowerShellUtilities.psm1
+++ PowerShellUtilities.psm1
@@ -1,22 +1,4 @@
 
-#Powershell utility library
-
-#	Please note, the following variables will need to be set in your own powershell profile at:
-#   	"C:\Users\<Username>\Documents\WindowsPowerShell\Microsoft.Powershell_profile.ps1"
-#
-#	Necessary:
-#		$DebugBrowser = "C:\Program Files (x86)\Internet Explorer\iexplore.exe";	#(Web browser to debug projects)
-#
-#	Optional:
-#		$npp = "C:\Program Files (x86)\Notepad++\notepad++.exe"; 					#(Favourite text editor, mine is notepad++)
-
-if(-not $(Import-Module WebAdministration -ErrorAction SilentlyContinue))
-{
-	Write-Output "WebAdministration module was not loaded successfully, please make sure that this feature is enabled in 'Programs and Features' -> 'Turn Windows features on or off' -> 'Internet Information Services' -> 'Web Management Tools'.";
-}
-
-$i = "IIS Admin Service";
-
 function Get-FunctionExists([string] $functionName)
 {
 	if(Get-Command $functionName -ErrorAction SilentlyContinue)
@@ -24,13 +6,6 @@ function Get-FunctionExists([string] $functionName)
 		return $true;
 	}
 	return $false;
-}
-
-function Set-DefaultAppPool32Bit([boolean]$value)
-{
-	$app = Get-Item IIS:\AppPools\DefaultAppPool;
-	$app.enable32BitAppOnWin64 = $value;
-	$app | Set-Item;
 }
 
 function StopProcess([string]$processName)
@@ -50,28 +25,4 @@ function RemoveLinesFromFile([string] $filename, [int] $startLine, [int] $endLin
 	$out += $content[($endLine+1)..($content.Count-1)];
 	$out | Out-File $filename;
 }
-function ExecuteSQL([string] $command, [string] $connectionString)
-{
-	$SqlConnection = New-Object System.Data.SqlClient.SqlConnection
-	$SqlConnection.ConnectionString = $connectionString
-
-	$SqlCmd = New-Object System.Data.SqlClient.SqlCommand
-	$SqlCmd.CommandText = $command
-	$SqlCmd.Connection = $SqlConnection
-
-	$SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
-	$SqlAdapter.SelectCommand = $SqlCmd
-
-	$DataSet = New-Object System.Data.DataSet
-	$SqlAdapter.Fill($DataSet)
-	 
-	$SqlConnection.Close()
-
-	return $DataSet.Tables[0];
-}
-function ExecuteSQL([string] $command, [string] $Server, [string] $DBName, [string] $SQLuser, [string] $SQLpass)
-{
-
-}
-
 $Host.UI.RawUI.WindowTitle = "Powershell with Dan's scripts: ";
