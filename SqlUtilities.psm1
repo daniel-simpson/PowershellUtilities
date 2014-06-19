14677ec135e95df9f8828b31f6619a96ce8bcc4a
Dan Simpson <daniel_simpson1988@hotmail.com>
2014-06-20 09:04:39 +1000

    Create SqlUtilities.psm1

---
+++ SqlUtilities.psm1
@@ -0,0 +1,26 @@
+
+function ExecuteSQL([string] $command, [string] $connectionString)
+{
+	$SqlConnection = New-Object System.Data.SqlClient.SqlConnection
+	$SqlConnection.ConnectionString = $connectionString
+
+	$SqlCmd = New-Object System.Data.SqlClient.SqlCommand
+	$SqlCmd.CommandText = $command
+	$SqlCmd.Connection = $SqlConnection
+
+	$SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
+	$SqlAdapter.SelectCommand = $SqlCmd
+
+	$DataSet = New-Object System.Data.DataSet
+	$SqlAdapter.Fill($DataSet)
+	 
+	$SqlConnection.Close()
+
+	return $DataSet.Tables[0];
+}
+function 
+ExecuteSQL([string] $command, [string] $Server, [string] $DBName, [string] $SQLuser, [string] $SQLpass)
+{
+	$connectionString = "Data Source="+$server+";Initial Catalog="+$DBName+";uid="+$SQLuser+";pass="+$SQLpass+";";
+	ExecuteSql($command, $connectionString);
+}
