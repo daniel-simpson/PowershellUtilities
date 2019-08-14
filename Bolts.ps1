1a290b4f16ca9cd1065d43dbf502ab38cc78056b
Daniel Simpson <daniel_simpson1988@hotmail.com>
2019-08-14 10:57:21 +1000

    Draw bolts with powershell

---
+++ Bolts.ps1
@@ -0,0 +1,29 @@
+param([int] $bolts = 1)
+
+1..$bolts | % { Write-Host -NoNewline '             zeeeeee-' }; Write-Host "";
+1..$bolts | % { Write-Host -NoNewline '            z$$$$$$" ' }; Write-Host "";
+1..$bolts | % { Write-Host -NoNewline '           d$$$$$$"  ' }; Write-Host "";
+1..$bolts | % { Write-Host -NoNewline '          d$$$$$P    ' }; Write-Host "";
+1..$bolts | % { Write-Host -NoNewline '         d$$$$$P     ' }; Write-Host "";
+1..$bolts | % { Write-Host -NoNewline '        $$$$$$"      ' }; Write-Host "";
+1..$bolts | % { Write-Host -NoNewline '      .$$$$$$"       ' }; Write-Host "";
+1..$bolts | % { Write-Host -NoNewline '     .$$$$$$"        ' }; Write-Host "";
+1..$bolts | % { Write-Host -NoNewline '    4$$$$$$$$$$$$$"  ' }; Write-Host "";
+1..$bolts | % { Write-Host -NoNewline '   z$$$$$$$$$$$$$"   ' }; Write-Host "";
+1..$bolts | % { Write-Host -NoNewline '   """""""3$$$$$"    ' }; Write-Host "";
+1..$bolts | % { Write-Host -NoNewline '         z$$$$P      ' }; Write-Host "";
+1..$bolts | % { Write-Host -NoNewline '        d$$$$"       ' }; Write-Host "";
+1..$bolts | % { Write-Host -NoNewline '      .$$$$$"        ' }; Write-Host "";
+1..$bolts | % { Write-Host -NoNewline '     z$$$$$"         ' }; Write-Host "";
+1..$bolts | % { Write-Host -NoNewline '    z$$$$P           ' }; Write-Host "";
+1..$bolts | % { Write-Host -NoNewline '   d$$$$$$$$$$"      ' }; Write-Host "";
+1..$bolts | % { Write-Host -NoNewline '  *******$$$"        ' }; Write-Host "";
+1..$bolts | % { Write-Host -NoNewline '       .$$$"         ' }; Write-Host "";
+1..$bolts | % { Write-Host -NoNewline '      .$$"           ' }; Write-Host "";
+1..$bolts | % { Write-Host -NoNewline '     4$P"            ' }; Write-Host "";
+1..$bolts | % { Write-Host -NoNewline '    z$"              ' }; Write-Host "";
+1..$bolts | % { Write-Host -NoNewline '   zP                ' }; Write-Host "";
+1..$bolts | % { Write-Host -NoNewline '  z"                 ' }; Write-Host "";
+1..$bolts | % { Write-Host -NoNewline ' /                   ' }; Write-Host "";
+1..$bolts | % { Write-Host -NoNewline '^                    ' }; Write-Host "";
+
